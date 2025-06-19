4ce5b7f774898b163d098a5bbf8bb5ec8961f35d
ll <3271928090@qq.com>
2025-06-19 12:03:05 +0800

    create base,character,enemy

---
+++ Source/Anima/Public/Character/Hero/AnimaCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AnimaCharacterBase.h"
+#include "AnimaCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMA_API AAnimaCharacter : public AAnimaCharacterBase
+{
+	GENERATED_BODY()
+	
+};
