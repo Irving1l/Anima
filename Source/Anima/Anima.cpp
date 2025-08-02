288a27844f18cdee248ef46650c2ed35bf410692
ll <3271928090@qq.com>
2025-08-03 00:00:13 +0800

    initial project files

---
+++ Source/Anima/Anima.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Anima.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Anima, "Anima" );
